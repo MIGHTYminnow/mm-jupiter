84808e848944b51f24f2fab2aeacb2adbfdf1890
jgonzo <jgonzo127@gmail.com>
2016-12-06 15:57:35 -0800

    Add skip links to keyboard nav

--- js/mmj-custom.js
+++ js/mmj-custom.js
@@ -2,11 +2,30 @@
  * Custom JS for Jupiter Theme, added by MIGHTYminnow
  */
 
+ //Callback to fix skip links focus.
+ function mmj_skiplinks() {
+     'use strict';
+     var element = document.getElementById( location.hash.substring( 1 ) );
+
+     if ( element ) {
+         if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+             element.tabIndex = -1;
+         }
+         element.focus();
+     }
+ }
+
 jQuery( document ).ready( function( $ ) {
 
 	 // Add classes to parts of lists.
 	 $( 'li:last-child' ).addClass( 'last' );
 	 $( 'li:first-child' ).addClass( 'first' );
 	 $( 'ul, ol' ).parent( 'li' ).addClass( 'parent' );
+
+	 if ( window.addEventListener ) {
+	     window.addEventListener( 'hashchange', mmj_skiplinks, false );
+	 } else { // IE8 and earlier.
+	     window.attachEvent( 'onhashchange', mmj_skiplinks, false );
+	 }
 
 })
