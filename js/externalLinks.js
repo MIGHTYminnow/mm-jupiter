da6b6193cbeb6e1c43b970a4cbb946d96d3fd82e
jordan gonzales <jordangonzales@jordans-MacBook-Air.local>
2018-03-23 10:26:53 -0700

    Prevent mailto links from opening in a new tab

--- js/externalLinks.js
+++ js/externalLinks.js
@@ -3,8 +3,9 @@ jQuery( document ).ready( function( $ ) {
 
 	// External Links.
 	var h = window.location.host.toLowerCase();
-	$( '[href^="http"]' ).not( '[href*="' + h + '"]' ).attr( "target", "_blank" );
+	$('[href^="http"]').not('[href*="' + h + '"]').attr( "target", "_blank" );
+	$('[href*="mailto:"]').attr("target", "_self");
 	$( 'a[href$=".pdf"]' ).attr({ "target":"_blank" });
 	$( 'a[href$=".doc"]' ).attr({ "target":"_blank" });
 
-})
+});
