0ef2c8591010328c24d6a168a7bcbe7994d12272
Aaron Barraza <aebs90@gmail.com>
2023-05-25 16:23:35 -0700

    Update the external links script to latest version from MM docs

--- js/externalLinks.js
+++ js/externalLinks.js
@@ -1,11 +1,22 @@
-// Script to handle external links.
-jQuery( document ).ready( function( $ ) {
-
-	// External Links.
+/**
+ * External Links
+ */
+( function( $ ) {
+	/** Open external links and files with extensions .pdf, .doc, .docx, .mp3, .m4a, and .wav in a new window. */
 	var h = window.location.host.toLowerCase();
-	$('[href^="http"]').not('[href*="' + h + '"]').attr( "target", "_blank" );
-	$('[href*="mailto:"]').attr("target", "_self");
-	$( 'a[href$=".pdf"]' ).attr({ "target":"_blank" });
-	$( 'a[href$=".doc"]' ).attr({ "target":"_blank" });
+	$(
+		"a[href^='http']:not([href*='" + h + "' i]), "
+		+ "form[action^='http']:not([action*='" + h + "' i]), "
+		+ "a[href$='.doc' i], "
+		+ "a[href$='.docx' i], "
+		+ "a[href$='.m4a' i], "
+		+ "a[href$='.mp3' i], "
+		+ "a[href$='.pdf' i], "
+		+ "a[href$='.wav' i]"
+	).attr( 'target', '_blank' );
 
-});
+	/** Add classes to Phone, PDF and E-mail links for easier use. */
+	$( 'a[href*=".pdf" i]' ).addClass( 'pdfLink' );
+	$( 'a[href^="mailto:" i]').addClass( 'emailLink' );
+	$( 'a[href^="tel:" i]').addClass( 'phoneLink' );
+} )( jQuery );
